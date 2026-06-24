1dbabfc400a3053edb5cd3cfbcf53536568d73f3
Dipak kokate <dk6939980@gmail.com>
2026-06-24 16:04:17 +0530

    Add EC2 instance resource configuration

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "devops" {
+  ami                    = "ami-0b6d9d3d33ba97d99"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0228cdd58be6dafc0"]
+  delete_on_termination  = true
+  volume_type            = "gp2"
+  volume_size            = 10
+
+
+  tags = {
+    Name     = "MyTerraformEC2"
+    purpose  = "webserverinstalled"
+  }
+}
